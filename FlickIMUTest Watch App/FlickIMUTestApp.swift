478b7fe6ed4e2d8699eaccdfc48beab014bc45e8
豊田 真吾 <toyodasinngo@gmail.com>
2025-01-27 01:47:37 +0900

    Initial Commit

---
+++ FlickIMUTest Watch App/FlickIMUTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickIMUTestApp.swift
+//  FlickIMUTest Watch App
+//
+//  Created by Shingo Toyoda on 2025/01/27.
+//
+
+import SwiftUI
+
+@main
+struct FlickIMUTest_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlickIMUTest Watch App/MLService.swift
@@ -0,0 +1,75 @@
+//
+//  MLService.swift
+//  FlickIMUTest
+//
+//  Created by Shingo Toyoda on 2025/01/27.
+//
+import CoreML
+
+class MLService {
+    
+    let shape = [1, 39, 16] as [NSNumber] // 入力の形状を指定
+    private var flickModel: FlickModel?
+    
+    init() {
+        // モデルのロード
+        do {
+            flickModel = try FlickModel(configuration: MLModelConfiguration())
+        } catch {
+            print("モデルのロードに失敗しました: \(error)")
+        }
+    }
+    
+    func predict(_ input: [[Double]]) -> MLResult? {
+        guard let inputArray = try? MLMultiArray(shape: shape, dataType: .double) else {
+            fatalError("MLMultiArrayの作成に失敗しました")
+        }
+        for i in 0..<39 {
+            for j in 0..<16 {
+                let index = [0, i, j] as [NSNumber]
+                let value = input[i][j]
+                inputArray[index] = NSNumber(value: value)
+            }
+        }
+        if let model = flickModel {
+            let input = FlickModelInput(conv1d_input: inputArray)
+            do {
+                let prediction:FlickModelOutput = try model.prediction(input: input)
+                let output = mlMultiArrayToDoubleArray(prediction.Identity)
+                //最大となるインデックスと値を取得
+                let maxValue = output.max() ?? 0
+                if let maxIndex = output.firstIndex(of: maxValue){
+                    return MLResult(label: AppGesture(rawValue: maxIndex) ?? .tap, probability: maxValue)
+                }
+                return nil
+            } catch {
+                print("推論に失敗しました: \(error)")
+                return nil
+            }
+        }
+        return nil
+    }
+    
+    func mlMultiArrayToDoubleArray(_ multiArray: MLMultiArray) -> [Double] {
+        let length = multiArray.count
+        let doublePtr =  multiArray.dataPointer.bindMemory(to: Float16.self, capacity: length)
+        let doubleBuffer = UnsafeBufferPointer(start: doublePtr, count: length)
+        let output = Array(doubleBuffer).compactMap { Double($0) }
+        return output
+    }
+        
+        
+}
+
+struct MLResult {
+    let label: AppGesture
+    let probability: Double
+}
+    
+enum AppGesture: Int {
+    case tap = 0
+    case up = 1
+    case down = 2
+    case left = 3
+    case right = 4
+}
